881f194fef1960ccd84e37c2e88ac807d6a92845
Nate Lang <lang.na@husky.neu.edu>
2017-08-08 14:11:17 -0400

    refactored config to work with heroku and added Procfile

---
+++ app/config/config.js
@@ -0,0 +1,8 @@
+const port = 3000;
+
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+module.exports.port = port;
+module.exports.spotifyClientId = spotifyClientId;
+module.exports.spotifyClientSecret = spotifyClientSecret;
