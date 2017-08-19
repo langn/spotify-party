8706daae98fb25bff34a02341f3969b9a1233657
Nate Lang <lang.na@husky.neu.edu>
2017-08-18 21:49:26 -0400

    Changed DB url

--- app/config/databse-config.js
+++ app/config/databse-config.js
@@ -6,7 +6,7 @@ if(process.env.MONGO_USER){ // check if running remotely
     const username = process.env.MONGO_USER; // get from environment
     const password = process.env.MONGO_PASS;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds013192.mlab.com:13192/heroku_q9g26xqw'; // user yours
+    connectionString += '@ds149373.mlab.com:49373/heroku_hkpbxhm1'; // user yours
 }
 
 const db = mongoose.connect(connectionString);
